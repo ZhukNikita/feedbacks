c8f2574284f8ca02c9ea804dcb294a66f2c813d6
ZhukNikita <48828173+ZhukNikita@users.noreply.github.com>
2022-09-09 19:26:06 +0300

    Add files via upload

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,8 @@
+import settings from './Settings.module.css'
+export default function Settings(){
+    return(
+        <div className={settings.body}>
+            <h2>Настройки</h2>
+        </div>
+    )
+}
